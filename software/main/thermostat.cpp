db551bee5b0b0a0435e1cf5e3b9c4b7d5923aca7
William Fries <william.james.fries@gmail.com>
2021-06-06 17:16:04 -0500

    Fix first on delay and short cycle checks in thermostat

--- software/main/thermostat.cpp
+++ software/main/thermostat.cpp
@@ -2,15 +2,19 @@
 #include "thermostat.h"
 #include "util.h"
 
+#define FIRST_ON_DELAY (1000UL * 10UL)
+
 static ulong last_transition = 0;
 static bool current_state = false;
+static bool turned_on_once = false;
 
 void therm_update(float actual, float desired) {
+    ulong t = millis();
 
     set_relay( current_state );
 
-    // Wait 10 seconds before turning on
-    if( millis() < 1000UL * 10UL ) return;
+    // Wait a fixed period before first turned on
+    if( t < FIRST_ON_DELAY ) return;
 
     // Calculate histeresis
     desired += ( current_state ? (1.5f) : (-1.5f) );
@@ -18,11 +22,13 @@ void therm_update(float actual, float desired) {
     bool new_state = actual < desired;
     if( new_state == current_state ) return;
     // If turning on, then we need to make sure we aren't short cycling
-    ulong t = millis();
-    if( new_state && t - last_transition < 1000UL * 45UL ) return;
+    // Skip short cycling check if this is the first turn-on
+    if( turned_on_once && new_state && t - last_transition < 1000UL * 45UL ) return;
 
     last_transition = t;
     current_state = new_state;
+    if( new_state )
+        turned_on_once = true;
     set_relay( current_state );
 
 }
